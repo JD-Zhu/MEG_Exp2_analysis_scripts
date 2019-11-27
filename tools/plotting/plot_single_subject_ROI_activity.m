5bdc7032d619b56c0097fe5a36b0d18bab357e1b
JD-Zhu <di.zhu@mq.edu.au>
2019-11-27 12:16:16 +1100

    Updated comments for plotting script.

--- tools/plotting/plot_single_subject_ROI_activity.m
+++ tools/plotting/plot_single_subject_ROI_activity.m
@@ -2,20 +2,17 @@
 % "Source activity" can be for an ROI (i.e. virtual sensor), or for a single vertex,
 % just load the desired source timecourse into the VE variable
 
-% Step 1: 
-% Load the ROI output file (e.g. A01-XC-3489_ROI.mat)
-% by dragging it into the workspace
-
-% Step 2:
-% SELECT which ROI to plot (or you can loop thru all ROIs)
-VE = ROI_activity.LIFG;
-eventnames_real = fieldnames(VE);
+% SELECT which ROI/vertex to plot
+VE = ROI_activity.LIFG; % load the ROI output file (e.g. A01-XC-3489_ROI.mat) first
+VE = singleVertex50; % load the saved timecourses (e.g. singleVertex_timecourse_in_A01_LIFG.mat) first
 
 % SELECT which conds to plot:
 %conds = 1:9; % plot all conds
 conds = [1 3 4 6]; 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+eventnames_real = fieldnames(VE);
+
 figure; hold on; 
 for j = conds
    plot(VE.(eventnames_real{j}).time, VE.(eventnames_real{j}).avg);
