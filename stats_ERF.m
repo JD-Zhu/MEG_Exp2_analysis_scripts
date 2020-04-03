048ed34f0359c75b2fc40b97c096fd5c04004e3c
JD-Zhu <di.zhu@mq.edu.au>
2020-04-03 19:19:50 +1100

    Misc update on choosing which neighbours file to use

--- stats_ERF.m
+++ stats_ERF.m
@@ -244,7 +244,8 @@ fprintf('\n= STATS: CLUSTER-BASED PERMUTATION TESTS =\n');
 
 cfg = [];
 cfg.channel   = {'all', '-AG101', '-AG122', '-AG007', '-AG103'}; % remove noisy sensors (see above)
-load('neighbours_dist5.mat'); % this is the same for all subjects (even same across experiments). So just load the prepared file.
+%load('neighbours_tri.mat'); % obtained using 'trigangulation' method in ft_prepare_neighbour
+load('neighbours_dist5.mat'); % obtained using 'distance' method, with a distance threshold of 5 (default is 4)
 cfg.neighbours = neighbours;  % same as defined for the between-trials experiment
 
 % can choose diff time windows to analyse for cue epochs & target epochs
